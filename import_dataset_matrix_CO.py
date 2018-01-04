7f988b0ec6b8734dc0e30a5ba305906456584b82
Hanbin Seo <myhb9188@seoultech.ac.kr>
2018-01-05 01:30:59 +0900

    modify matrix co-classification

--- import_dataset_matrix_CO.py
+++ import_dataset_matrix_CO.py
@@ -17,7 +17,6 @@ if __name__ == '__main__':
     period = int(input("Which period ? (09~11=1, 12~14=2, 15~17=3): "))
     matrix_W = pd.read_pickle('./data/matrix_data/matrix_W_p{}.pickle'.format(period))
     header = matrix_W.columns.values
-    print(matrix_W.head())
     print(matrix_W.shape)
     
     matrix_W = matrix_W.values
