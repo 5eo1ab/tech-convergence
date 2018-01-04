2a4c8d714662e60aac0596199eef6f3c1ff984b3
Hanbin Seo <myhb9188@seoultech.ac.kr>
2018-01-04 18:24:31 +0900

    modify minor update

--- import_dataset_mod1.py
+++ import_dataset_mod1.py
@@ -7,7 +7,7 @@ Created on Thu Jan  4 13:55:17 2018
 
 import os
 os.getcwd()
-
+#os.chdir("D:/Paper_2018/tech-convergence")
 import pandas as pd
 from pandas import DataFrame as df
 
@@ -16,6 +16,13 @@ def lambda_str_class(str_x):
         return "0{}".format(str_x)
     else: return str_x
     
+def lambda_split_period(issue_year):
+    if issue_year < 2012:
+        return 1
+    elif issue_year > 2014:
+        return 3
+    else: return 2
+
 if __name__ == '__main__':
 
     t_class = pd.read_csv('./data/raw_data/t_class.csv')
@@ -23,13 +30,12 @@ if __name__ == '__main__':
     print(t_class.head())
 
     t_patent = pd.read_csv('./data/raw_data/t_patent.csv')
-    t_patent.head()
-
+    print(t_patent.head())
    
     t_class_mod1 = t_class[['patent_no', '_sector']]
     t_class_mod1.columns = ['patent_no', 'sector']
-    t_class_mod1['class'] = t_class['_sector'] + t_class['str_class']
-    t_class_mod1['subclass'] = t_class['_sector'] + t_class['str_class'] + t_class['_subclass']
+    t_class_mod1['class'] = t_class['_sector'].map(str.upper) + t_class['str_class']
+    t_class_mod1['subclass'] = t_class['_sector'].map(str.upper) + t_class['str_class'] + t_class['_subclass'].map(str.upper)
     print(t_class_mod1.tail())
     print(t_class_mod1.shape)  # (6780633, 4)
    
@@ -38,7 +44,9 @@ if __name__ == '__main__':
     print(t_class_mod2.shape)  # (3692503, 4)  
 
     t_class_mod3 = pd.merge(t_class_mod2, t_patent, on='patent_no', how='left')
-    t_class_mod3.head()
+    t_class_mod3['period'] = t_class_mod3['issue_year'].apply(lambda_split_period)
+    print(t_class_mod3.head())
+    print(t_class_mod3.tail())
     
     t_class_mod3.to_csv('./data/raw_data/merged_t_class.csv', index=False)
 
