ff2913b92b08cdb3195c08e21ef26698303b2efe
Hanbin Seo <myhb9188@seoultech.ac.kr>
2018-01-09 01:32:04 +0900

    modify merging script

--- import_dataset_merging.py
+++ import_dataset_merging.py
@@ -6,16 +6,18 @@ Created on Thu Jan  4 13:55:17 2018
 """
 
 import os
-os.getcwd()
+print(os.getcwd())
 #os.chdir("D:/Paper_2018/tech-convergence")
+import json
 import pandas as pd
 from pandas import DataFrame as df
+
+DIC_UNIT = json.load(open('./data/digit-unit.json'))
 
 def lambda_str_class(str_x):
     if len(str_x) < 2:
         return "0{}".format(str_x)
     else: return str_x
-    
 def lambda_split_period(issue_year):
     if issue_year < 2012:
         return 1
@@ -25,8 +27,9 @@ def lambda_split_period(issue_year):
 
 if __name__ == '__main__':
 
-    obj_unit = input("Which merging unit?\n(sector=1, class=2, subclass=3, group=4, subclass=5): ")
-    merging_unit = {'1': 'sector', '2': 'class', '3': 'subclass', '4': 'group', '5': 'subclass'}
+    #obj_unit = input("Which merging unit?\n(sector=1, class=2, subclass=3, group=4, subclass=5): ")
+    #merging_unit = {'1': 'sector', '2': 'class', '3': 'subclass', '4': 'group', '5': 'subclass'}
+    obj_unit = input(DIC_UNIT['message'])
 
     t_class = pd.read_csv('./data/raw_data/t_class.csv')
     t_class['str_class'] = t_class['_class'].map(str).apply(lambda_str_class)
@@ -45,12 +48,13 @@ if __name__ == '__main__':
     t_class_mod1['subgroup'] = t_class_mod1['group'] + ['/']*len(t_class_mod1) + t_class['str_subgroup']
     print(t_class_mod1.shape)
 
-    t_class_mod2 = t_class_mod1.drop_duplicates(subset=['patent_no', merging_unit[obj_unit]], keep='first')
+    t_class_mod2 = t_class_mod1.drop_duplicates(subset=['patent_no', DIC_UNIT[obj_unit]], keep='first')
     print(t_class_mod2.shape)
     
     t_class_mod3 = pd.merge(t_class_mod2, t_patent, on='patent_no', how='left')
     t_class_mod3['period'] = t_class_mod3['issue_year'].apply(lambda_split_period)
     print(t_class_mod3.shape)
-    print(t_class_mod3.tail())
 
     t_class_mod3.to_csv('./data/raw_data/t_class_merging.csv'.format(), index=False)
+    print(t_class_mod3.tail())
+
